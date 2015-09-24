fc6b789a22149bcab22d5cef9041722e9dbfd2f8
Usha09ec125 <usha.wadhwani@gatewaytechnolabs.com>
2015-09-24 15:45:31 +0530

    Update loginController.js

--- Angular/Angular/loginController.js
+++ Angular/Angular/loginController.js
@@ -1,7 +1,7 @@
 ﻿angular.module('myApp.Controller', [])
     .controller('loginController', function ($scope) {        
         $scope.CheckLoginUser = function () {        
-        if ($scope.username.Trim() == "nosql" && $scope.password.Trim() == "html5") {
+        if ($scope.username == "nosql" && $scope.password== "html5") {
                 alert('Welcome to hell');
             }
             else {
