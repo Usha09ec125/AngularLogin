46166835ba30d6d9760fc1221b0c2d06acb8b8a2
Usha09ec125 <usha.wadhwani@gatewaytechnolabs.com>
2015-09-24 13:33:01 +0530

    'FirstCommit'

---
+++ Angular/Angular/loginController.js
@@ -0,0 +1,11 @@
+﻿angular.module('myApp.Controller', [])
+    .controller('loginController', function ($scope) {        
+        $scope.CheckLoginUser = function () {        
+        if ($scope.username.Trim() == "nosql" && $scope.password.Trim() == "html5") {
+                alert('Welcome to hell');
+            }
+            else {
+            alert('You cannot survive in hell :D');
+            }
+        }
+    })
---
+++ Angular/Angular/myApp.js
@@ -0,0 +1,22 @@
+﻿angular.module('myApp', ['ui.router', 'myApp.Controller'])
+.config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state('home', {
+        url: '/home',
+        templateUrl: 'templates/home.html'
+        })
+    .state('aboutus', {
+        url: '/aboutus',
+        templateUrl: 'templates/aboutus.html'
+    })
+    .state('contactus', {
+        url: '/contactus',
+        templateUrl: 'templates/contactus.html'
+    })
+    .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller:'loginController'
+    })
+}])
